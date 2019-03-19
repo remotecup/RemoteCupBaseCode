558a907944c75208c8913e42ec77303ff72715b4
Nader Zare <nader.zare88@gmail.com>
2019-03-19 14:40:07 +0330

    change in client, work with u d l r

--- Games/Simple/Client/Python/Client.py
+++ Games/Simple/Client/Python/Client.py
@@ -4,6 +4,13 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_address = ('localhost', 20002)
 
-a = {'message_type':'connect', 'value':{'name':'mahtab'}}
-
+a = {'message_type': 'connect', 'value': {'name': input()}}
 sock.sendto(str.encode(str(a)), server_address)
+r = sock.recvfrom(1024)
+print(str(r[0].decode("utf-8")))
+while True:
+    r = sock.recvfrom(1024)
+    print(str(r[0].decode("utf-8")))
+    a = {'message_type': 'action', 'value': {'name': input()}}
+    sock.sendto(str.encode(str(a)), server_address)
+
