0e513a5e17824cf71054d8670cf8eaa8cd6497df
Nader Zare <nader.zare88@gmail.com>
2019-04-09 01:07:16 +0430

    update snake client

--- Games/Snake/Client/Python/World.py
+++ Games/Snake/Client/Python/World.py
@@ -1,5 +1,30 @@
 from Base.Math import *
 
+class Snake:
+    def __init__(self, id):
+        self.id = id
+        self.head = None
+        self.body = []
+
+    def get_body(self):
+        return self.body
+
+    def add_body(self, pos):
+        self.body.append(pos)
+
+    def get_head(self):
+        return self.head
+
+    def set_head(self, pos):
+        self.head = pos
+
+    def get_id(self):
+        return self.id
+
+    def reset(self):
+        self.head = None
+        self.body.clear()
+
 
 class World:
     def __init__(self):
@@ -7,8 +32,11 @@ class World:
         self.cycle = None
         self.self_id = None
         self.goal_id = None
-        self.self_position = None
         self.goal_position = None
+        self.snakes = {}
+        for s in range(1, 5):
+            self.snakes[s] = Snake(s)
+        self.walls = []
 
     def set_id(self, self_id, goal_id):
         self.self_id = self_id
@@ -17,19 +45,37 @@ class World:
     def update(self, message):
         self.board = message.world['board']
         self.cycle = message.cycle
-        print(self.cycle)
-        self.self_position = Vector2D(message.world['heads'][self.self_id][0], message.world['heads'][self.self_id][1])
-        print(self.self_position)
+        self.walls.clear()
+        for s in self.snakes:
+            self.snakes[s].reset()
 
         for i in range(len(self.board)):
             for j in range(len(self.board[i])):
                 if self.board[i][j] == self.goal_id:
                     self.goal_position = Vector2D(i, j)
+                elif self.board[i][j] > 0:
+                    self.snakes[self.board[i][j]].add_body(Vector2D(i, j))
+                elif self.board[i][j] == -1:
+                    self.walls.append(Vector2D(i, j))
 
-        print('g id', self.goal_id, self.goal_position)
+        for s in self.snakes:
+            self.snakes[s].set_head(Vector2D(message.world['heads'][s][0], message.world['heads'][s][1]))
+
+    def get_self(self):
+        return self.snakes[self.self_id]
+
+    def get_snake(self, id):
+        return self.snakes[self.id]
+
+    def get_walls(self):
+        return self.walls
 
     def print(self):
+        print('------------------------------------')
         print('cycle: {}'.format(self.cycle))
         for f in self.board:
             print(f)
+        print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+        for s in self.snakes:
+            print(self.snakes[s].get_id(), self.snakes[s].get_head(), self.snakes[s].get_body())
         print('------------------------------------')
